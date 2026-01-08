2d64e42149f7350271350da11f61faae32734c49
brunovittoria <Bruno.Vittoria@umbrage.com>
2026-01-07 21:14:58 -0300

    Refactor Task model in Prisma schema for consistency, add PaginationDto for handling pagination in tasks, and update getTasks method in TasksController to utilize pagination parameters.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,18 +7,20 @@ import {
   Delete,
   Patch,
   ParseIntPipe,
+  Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { PaginationDto } from '../common/dto/pagination.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly taskService: TasksService) {}
 
   @Get()
-  getTasks() {
-    return this.taskService.listAll();
+  getTasks(@Query() paginationDto: PaginationDto) {
+    return this.taskService.listAll(paginationDto);
   }
 
   @Get(':id')
